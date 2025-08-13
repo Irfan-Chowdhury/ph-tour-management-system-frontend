ca3da011831eae144572107c5745d9529f9483c2
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-08-13 11:29:46 +0600

    36-1 Implementing Dark Theme Toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
+import React from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
 import { RouterProvider } from 'react-router'
 import { router } from './routes/index.tsx'
+import { ThemeProvider } from "./providers/theme.provider.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+  <React.StrictMode>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router}/>
+      </ThemeProvider>
+    
+  </React.StrictMode>,
 )
